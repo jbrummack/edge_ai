96ceb587b5470ee0451bf181e9220ddeb6d3d6d5
Julius Brummack <133630819+jbrummack@users.noreply.github.com>
2026-06-06 00:47:16 +0200

    pass through with download of debug syms

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 use edge_ai::embedders::vision::vmodel::VModel;
 
 fn main() {
+    unsafe {
+        executorch::platform::pal_init();
+    }
+
     let mut model = VModel::<448>::new("./models/tips_v2_coreml_q8.pte").unwrap();
     let image = image::open("./models/red-apple.jpg").unwrap();
     let emb = model.embed(image);
